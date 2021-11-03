efc04dd47b80588b64913ebeed4bf8e889efdfd7
Yash Poojary <yashspoojary245@gmail.com>
2021-11-03 19:07:11 +0530

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Yash Poojary on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
